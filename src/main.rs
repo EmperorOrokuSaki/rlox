81e524593532cb05d08e144b6335d7fedd5aca0a
EmperorOrokuSaki <artie.eth@gmail.com>
2024-08-14 20:45:06 +0400

    feat: cli tool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "rLox")]
+#[command(version = "1.0")]
+#[command(about = "A Lox compiler written in Rust", long_about = None)]
+struct Cli {
+    #[arg(long)]
+    path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("two: {:?}", cli.path);
 }
